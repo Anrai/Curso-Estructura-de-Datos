3f41a2a2408cb14ef5c81b324a2e521a4dcde06e
Sergio Enrique <anrai@me.com>
2014-02-24 08:35:00 -0600

    Estructura de nodo para tarea

--- Structs/StructNodo.cpp
+++ Structs/StructNodo.cpp
@@ -42,11 +42,11 @@ struct nodo llena_nodo()
 {
 	struct nodo n;
 
-	cout << endl << "Ingrese un numero entero: ";
+	cout << "Ingrese un numero entero: ";
 	cin >> n.num;
-	cout << endl << "Ingrese un flotante: ";
+	cout << "Ingrese un flotante: ";
 	cin >> n.s;
-	cout << endl << "Ingrese el nombre: ";
+	cout << "Ingrese el nombre: ";
 	cin >> n.nom;
 
 	return n;
@@ -57,16 +57,48 @@ struct nodo llena_nodo()
 //---------------------------------------------------------------------------------------
 int main(int argc, char** argv) {
 
-	struct nodo n1, n2;
+	struct nodo n1, n2, g[20];
+	int opcion, na=0, i;
 
-	n1=llena_nodo();
-	print_nodo(n1);
+	// Menú de opciones que siempre se muestran
+	while(1){
+		cout << "Que operacion quiere realizar en el arreglo de nodos? " << endl << "1 para insertar, 2 para mostrar, 3 para eliminar ";
+		cin >> opcion;
 
-	n2=llena_nodo();
-	print_nodo(n2);
+		switch(opcion)
+		{
+			case 1:
+				cout << endl << "-Se le pediran los valores del nuevo nodo: " << endl;
+				g[na]=llena_nodo();
+				na+=1;
+				break;
+			case 2:
+				cout << endl << "-Se mostraran todos los nodos:" << endl;
+				for(i=0; i<na; ++i)
+				{
+					cout << endl << "Nodo: " << endl;
+					print_nodo(g[i]);
+				}
+				break;
+			case 3:
+				cout << endl << "-Indique que numero de nodo quiere eliminar: ";
+				break;
+			default:
+				cout << "-La opcion especificada no existe, intente nuevamente." << endl;
+		}
+
+		cout << endl << "=====================================" << endl << endl;
+	}
 
 	// Fin del programa
 	cout << "\n\n";
 	system("pause");
 	return 0;
 }
+
+
+// TAREA PARA EL 26 DE FEBRERO:
+//		Terminar cola para caracteres
+//		Hacer un menú con las opciones de: insertar, mostrar y eliminar struct nodo dentro de un arreglo de nodo según su num
+//		muestra: muestra todo el arreglo
+//		inserta: inserta un elemento al final del arreglo
